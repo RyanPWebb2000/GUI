13a91460ddd98995b0f705205d2f9488ece77c36
RyanPWebb2000 <Ryan.P.Webb-1@ou.edu>
2020-11-18 16:32:00 -0600

    GUI created

---
+++ src/GUITest.java
@@ -0,0 +1,40 @@
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+public class GUITest {
+
+	private static final int HEIGHT = 400;
+	private static final int WIDTH = 300;
+
+	public static void main(String[] args) {
+		// So far, all of our programs were sequential and 
+		// have reached the end.
+		
+		JFrame frame =new JFrame("Dark wave");
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setLocationRelativeTo(null);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		FlowLayout layout = new FlowLayout();
+		frame.setLayout(layout);
+		
+		JButton b1 = new JButton("Man who stare");
+		JButton b2 = new JButton("at goats");
+		JButton b52 = new JButton("at code monkeys");
+		JButton doNotPushMe = new JButton(" my finger is on the button");
+		
+		frame.add(b1);
+		frame.add(b2);
+		frame.add(b52);
+		frame.add(doNotPushMe);
+		
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+
+}
